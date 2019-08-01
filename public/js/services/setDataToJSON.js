1f66118c932fab241b807fa8f9d201fd8d6c7178
Carlos Escario Bajo <carlosescariobajo@gmail.com>
2019-08-01 20:05:03 +0200

    file saver

--- public/js/services/setDataToJSON.js
+++ public/js/services/setDataToJSON.js
@@ -4,7 +4,7 @@ export function setDataToJSON(data) {
     const FileSaver = require('file-saver');
     const fs = require('fs');
     
-    var file = new File([data], "/home/cescari/grive/data.json", {type: "text/json;charset=utf-8"});
+    var file = new File([data], "data.json", {type: "text/json;charset=utf-8"});
     FileSaver.saveAs(file); 
 
    
